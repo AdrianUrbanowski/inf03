54709815155247f13eb1efa0abaff554676355c5
Adrian Urbanowski <adrianurbanowski2174@wp.pl>
2024-05-16 13:10:15 +0200

    Zadanie EE.09-01-22.01-SG

---
+++ EE.09-01-22.01-SG/zapisz.php
@@ -0,0 +1,14 @@
+<?php
+    $id_polaczenia = mysqli_connect("localhost", "root", "", "wedkowanie");
+
+    // Pobranie danych z formularza
+    $imie = $_POST['first_name'];
+    $nazwisko = $_POST['last_name'];
+    $adres = $_POST['address'];
+
+    $zapytanie = 'INSERT INTO `karty_wedkarskie`(imie, nazwisko, adres, data_zezwolenia, punkty) VALUES ("' . $imie . '" , "' . $nazwisko . '" , "' . $adres . '" , NULL , NULL)'; 
+    // wysłanie zapytania i wykonanie
+    mysqli_query($id_polaczenia, $zapytanie);
+    // rozłączenie z bazą
+    mysqli_close($id_polaczenia); 
+?>
